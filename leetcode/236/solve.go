2b99f32e1ba428809d527866453ea82f07e83f51
jaggesher <jaggesher14@gmail.com>
2021-08-16 22:54:44 +0600

    236. Lowest Common Ancestor of a Binary Tree

---
+++ leetcode/236/solve.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("No Test Case")
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left, right := lowestCommonAncestor(root.Left, p, q), lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	if right != nil {
+		return right
+	}
+	return nil
+}
