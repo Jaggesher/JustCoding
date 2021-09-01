e598c82be78440150c3259b0fb1d7ab6b3b08028
Jaggesher <jaggesher14@gmail.com>
2021-09-01 23:04:21 +0600

    230. Kth Smallest Element in a BST

---
+++ leetcode/230/solve.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("No test case")
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	var ans int
+	bfs(root, k, 0, &ans)
+	return ans
+}
+
+func bfs(node *TreeNode, k, soFar int, ans *int) int {
+	if node == nil || k <= soFar {
+		return soFar
+	}
+	soFar = bfs(node.Left, k, soFar, ans)
+	if (soFar + 1) == k {
+		*ans = node.Val
+	}
+	return bfs(node.Right, k, soFar+1, ans)
+}
