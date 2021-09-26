5d83061e41b1215f4f3ad05ab142ce953d6cc96c
Jaggesher <jaggesher14@gmail.com>
2021-09-27 00:00:06 +0600

    289. Game of Life

---
+++ leetcode/289/solve.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Case 1:")
+	var arr [][]int = [][]int{{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}}
+	fmt.Println(arr)
+	gameOfLife(arr)
+	fmt.Println(arr)
+}
+
+func gameOfLife(board [][]int) {
+	var dirs [8][2]int = [8][2]int{{-1, -1}, {1, 1}, {0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, -1}, {-1, 1}}
+	for i, row := range board {
+		for j, vl := range row {
+			neighbors := 0
+			for _, dir := range dirs {
+				u, v := i+dir[0], j+dir[1]
+				if u < 0 || v < 0 || u >= len(board) || v >= len(row) {
+					continue
+				}
+				neighbors += (board[u][v] % 2)
+			}
+			if vl == 1 && (neighbors < 2 || neighbors > 3) {
+				board[i][j] = 3
+			} else if vl == 0 && neighbors == 3 {
+				board[i][j] = 2
+			}
+		}
+
+	}
+
+	for i, row := range board {
+		for j, vl := range row {
+			if vl == 2 {
+				board[i][j] = 1
+			} else if vl == 3 {
+				board[i][j] = 0
+			}
+		}
+	}
+}
