30a832fa1a8decc1553d833a5fba3616e2bf561e
Jaggesher Mondal <jaggesher14@gmail.com>
2021-12-20 22:55:30 +0600

    61. Rotate List

---
+++ leetcode/61/solve.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("No test case")
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil || k == 0 {
+		return head
+	}
+	tail, count := head, 1
+	for tail.Next != nil {
+		tail = tail.Next
+		count++
+	}
+	k = (count - (k % count)) // Cut point
+	tail.Next = head          // Making ring
+
+	for k > 1 {
+		head = head.Next
+		k--
+	}
+
+	tail = head.Next
+	head.Next = nil
+	return tail
+}
