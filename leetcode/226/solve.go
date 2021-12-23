0bd764728b9964ced31fbc3a6c426ecd7a920261
Jaggesher Mondal <jaggesher14@gmail.com>
2021-12-24 02:03:00 +0600

    226. Invert Binary Tree

---
+++ leetcode/226/solve.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("No test case")
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/***
+ * Time:  O(n)
+ * Space: O(n)
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
