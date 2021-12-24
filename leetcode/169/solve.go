17b9fa6ebe9d4b70f75120f3896c5142243e8a7f
Jaggesher Mondal <jaggesher14@gmail.com>
2021-12-24 17:58:09 +0600

    169. Majority Element

---
+++ leetcode/169/solve.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Case 1:", majorityElement([]int{3, 2, 3}))
+	fmt.Println("Case 2:", majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
+}
+
+func majorityElement(nums []int) int {
+	var majority, count int = nums[0], 1
+	for i := 1; i < len(nums); i++ {
+		if majority == nums[i] {
+			count++
+		} else {
+			count--
+			if count == 0 {
+				majority, count = nums[i], 1
+			}
+		}
+	}
+	return majority
+}
