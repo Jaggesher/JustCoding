8174b9ec1500dab46c1214c5963d318de38b80a5
Jaggesher Mondal <jaggesher14@gmail.com>
2022-02-07 23:27:20 +0600

    253. Meeting Rooms II

--- leetcode/253/solve.go
+++ leetcode/253/solve.go
@@ -11,6 +11,31 @@ func main() {
 	fmt.Println("Case 2:", minMeetingRooms([][]int{{7, 10}, {2, 4}}))
 }
 
+/***
+ * Time: O(n log n) + O(n) => O(n log n)
+ * Space: O(n)
+ */
+func minMeetingRooms(intervals [][]int) int {
+	var starts, ends []int = make([]int, len(intervals)), make([]int, len(intervals))
+	for index, vl := range intervals {
+		starts[index], ends[index] = vl[0], vl[1]
+	}
+	sort.Ints(starts)
+	sort.Ints(ends)
+	var ans, ptr, usedRooms int = 1, 0, 1
+	for i := 1; i < len(starts); i++ {
+		for ends[ptr] <= starts[i] && usedRooms > 0 {
+			ptr++
+			usedRooms--
+		}
+		usedRooms++
+		if ans < usedRooms {
+			ans = usedRooms
+		}
+	}
+	return ans
+}
+
 type PQ []int
 
 func (h PQ) Len() int           { return len(h) }
@@ -27,7 +52,7 @@ func (h *PQ) Pop() interface{} {
 	return x
 }
 
-func minMeetingRooms(intervals [][]int) int {
+func minMeetingRooms1(intervals [][]int) int {
 	var ans int = 0
 	sort.Slice(intervals, func(i, j int) bool { return intervals[i][0] < intervals[j][0] })
 	pq := &PQ{}
